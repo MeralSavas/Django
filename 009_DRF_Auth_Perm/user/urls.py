393a7ecec41ab833495db632e9f3ec0d03c18776
MeralSavas <meralsavas@gmail.com>
2022-12-31 01:45:04 +0000

    register

--- 009_DRF_Auth_Perm/user/urls.py
+++ 009_DRF_Auth_Perm/user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-# from rest_framework.authtoken import views
+from rest_framework.authtoken import views
 
 from .views import RegisterView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
-    # path('login/', views.obtain_auth_token),
+    path('login/', views.obtain_auth_token),
     # path('logout/', logout),
 ]
