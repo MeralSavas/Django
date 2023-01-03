55faa2f54b4184747894533e4e2282163fa1f680
MeralSavas <meralsavas@gmail.com>
2023-01-03 17:07:13 +0000

    blog app

---
+++ 010_BlogApp/blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category
+
+
+class CategoryView(ModelViewSet): #  crud
+    queryset = Category.objects.all()
+    serializer_class = None
