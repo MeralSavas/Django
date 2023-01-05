1da2fe36cc97a8649d28da95af1906ad530c8884
MeralSavas <meralsavas@gmail.com>
2023-01-05 16:38:56 +0000

    fixed

--- 012_FlightApp_Auth/main/settings/base.py
+++ 012_FlightApp_Auth/main/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -214,3 +214,4 @@ REST_AUTH_SERIALIZERS = {
     'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
     
 }
+
