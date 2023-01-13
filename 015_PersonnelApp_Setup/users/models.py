f791b0875c3815b7f6383d37637b250255ad7874
MeralSavas <meralsavas@gmail.com>
2023-01-13 23:17:27 +0000

    add profile

--- 015_PersonnelApp_Setup/users/models.py
+++ 015_PersonnelApp_Setup/users/models.py
@@ -14,3 +14,13 @@ from django.contrib.auth.models import User
   
 #   def __str__(self):
 #       return "{}".format(self.email)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=30, blank=True, null=True)
+    avatar = models.ImageField(upload_to="pictures", default="avatar.png")
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile'"
