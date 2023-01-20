4218f922222ff0f1036a1fa31e7f830c1ec56c31
MeralSavas <meralsavas@gmail.com>
2023-01-20 10:06:57 +0000

    admin

--- 018_Admin_site/products/admin.py
+++ 018_Admin_site/products/admin.py
@@ -1,7 +1,52 @@
 from django.contrib import admin
 from .models import Product
+from django.utils import timezone
 
-admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","create_date", "is_in_stock", "update_date", "added_days_ago",)
+    list_editable = ( "is_in_stock",)
+    # list_display_links = ("create_date",)
+    list_filter = ("is_in_stock", "create_date")
+    # ordering = ("-update_date",)
+    search_fields = ("name",)
+    prepopulated_fields = {'slug' : ('name',)}
+    list_per_page = 25
+    date_hierarchy = "update_date"
+    # fields = (('name', 'slug'), 'description', "is_in_stock")
+    # inlines = (ReviewInline,)
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('name',"slug"), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('My section', {
+            "classes" : ("collapse", ),
+            "fields" : ("description",),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+
+    actions = ("is_in_stock", )
+
+    def is_in_stock(self, request, queryset):
+        count = queryset.update(is_in_stock=True)
+        self.message_user(request, f"{count} çeşit ürün stoğa eklendi") 
+
+    is_in_stock.short_description = 'İşaretlenen ürünleri stoğa ekle'
+
+    def added_days_ago(self, product):
+        fark = timezone.now() - product.create_date
+        return fark.days
+    
+	
+
+
+admin.site.register(Product, ProductAdmin)
 
 admin.site.site_title = "Clarusway Title"
 admin.site.site_header = "Clarusway Admin Portal"  
--- 018_Admin_site/products/models.py
+++ 018_Admin_site/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -14,4 +15,9 @@ class Product(models.Model):
         verbose_name_plural = "Products"
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.description}"
+
+
+    # def added_days_ago(self):
+    #     fark = timezone.now() - self.create_date
+    #     return fark.days
