744cd84d76c640143ba0bf84810cba9995d79f3e
MeralSavas <meralsavas@gmail.com>
2023-01-26 19:43:05 +0000

    car url

--- 019_RentACar_App/car/views.py
+++ 019_RentACar_App/car/views.py
@@ -1,14 +1,26 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Car, Reservation
 from .serializers import CarSerializer
+from .permissions import IsStaffOrReadOnly
 
 
 
 class CarView(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-    permission_classes = None
+    permission_classes = (IsStaffOrReadOnly,) #[isStaffOrReadOnly]
 
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            queryset = super().get_queryset()
+        else:
+            queryset = super().get_queryset().filter(availability=True)
+        start = self.request.query_params.get('start')
+        print(start)
+        end = self.request.query_params.get('end')
+        print(end)
+
+        return queryset
 
 
 
