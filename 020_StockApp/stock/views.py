00c5da5813d5c4f6e865a4c7f76b159833932c06
MeralSavas <meralsavas@gmail.com>
2023-02-03 13:40:12 +0000

    category product

--- 020_StockApp/stock/views.py
+++ 020_StockApp/stock/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
 from .models import Category, Brand, Product, Firm, Purchases, Sales
-from .serializers import CategorySerializer
+from .serializers import CategorySerializer, CategoryProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 class CategoryView(viewsets.ModelViewSet):
@@ -10,5 +10,11 @@ class CategoryView(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter,DjangoFilterBackend]
     search_fields = ['name']
     filterset_fields = ["name"]
+
+
+    def get_serializer_class(self):
+        if self.request.query_params.get("name"):
+            return CategoryProductSerializer
+        return super().get_serializer_class()
  
 
