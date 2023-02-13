231be9ec6dc2cdf38d0070e5a14c614c4855a40d
MeralSavas <meralsavas@gmail.com>
2023-02-13 00:40:39 +0000

    add product

--- 020_StockApp/stock/serializers.py
+++ 020_StockApp/stock/serializers.py
@@ -12,12 +12,23 @@ class CategorySerializer(serializers.ModelSerializer):
         return Product.objects.filter(category_id=obj.id).count()
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    category = serializers.StringRelatedField()
+    brand = serializers.StringRelatedField()
     
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = (
+            "id",
+            "name",
+            "category",
+            "category_id",
+            "brand",
+            "brand_id",
+            "stock",
+        )
 
-
+        read_only_fields = ("stock",) 
 class CategoryProductSerializer(serializers.ModelSerializer):
     
     products = ProductSerializer(many=True)
