e6f2703d514428cf05351370229e70cb2292fc8d
MeralSavas <meralsavas@gmail.com>
2023-02-13 22:57:41 +0000

    url.py

--- 021_Templates/main/settings.py
+++ 021_Templates/main/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'students',
+
+    #third app
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- 021_Templates/students/views.py
+++ 021_Templates/students/views.py
@@ -5,4 +5,6 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, )
+    # return render(request, 'home.html')
+
+    return HttpResponse('<p> Hello </p>')
