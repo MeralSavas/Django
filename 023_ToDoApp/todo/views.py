2e44bc20af61636cf9ad8b5c1f492342b444bd85
MeralSavas <meralsavas@gmail.com>
2023-02-15 17:53:25 +0000

    views

--- 023_ToDoApp/todo/views.py
+++ 023_ToDoApp/todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+from .forms import *
+
+def todo_list(request):
+    todos=Todo.objects.all()
+    context={
+        "todos":todos
+    }
+    return render(request,'list.html',context)
