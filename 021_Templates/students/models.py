490a4d925a1d4df5ed045f3c3b6a5036d810dd4f
MeralSavas <meralsavas@gmail.com>
2023-02-16 00:12:29 +0000

    models.py

--- 021_Templates/students/models.py
+++ 021_Templates/students/models.py
@@ -1,2 +1,26 @@
 from django.db import models
 
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.SmallIntegerField()
+    image = models.ImageField(upload_to='students', blank=True)
+
+    #upload_to bir yere kaydetmesini istiyorsak
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+
+
+
+
+
+
+
+#?------------- blank=True , null=True ----------------
+    # blank=True, serializer ile ilgilidir, yani boş bırakılabilir,
+    # null=True, DB ile ilgilidir, yani boş bırakılabilir ve DB null kayıt edilir,
+    #! eğer sadece  blank=True varsa veri boş gelebilir, ama DB kayıt edilmeden önce
+    #! bir işlem/hesaplama vs. yapılıp DB boş/null gitmesini önlemek gerekir.
