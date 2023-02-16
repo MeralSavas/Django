786b6ab4e1a4980f10c057535e2d1f09d45d4b8e
MeralSavas <meralsavas@gmail.com>
2023-02-16 18:48:12 +0000

    user form

--- 024_PizzaApp/users/views.py
+++ 024_PizzaApp/users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
 
 def register(request):
-    form = UserCreationForm()
+    # form = UserCreationForm()
+    form = UserForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        # form = UserCreationForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             # form sayfasında kalmasın, save olduktan sonra başka yere yönlendirilsin;
