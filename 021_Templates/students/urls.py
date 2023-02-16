efbb00b8404780938a7fce9a66745b66a5ea2fd3
MeralSavas <meralsavas@gmail.com>
2023-02-16 01:11:01 +0000

    student add

--- 021_Templates/students/urls.py
+++ 021_Templates/students/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from .views import (
     home,
-    student_list
+    student_list,
+    student_add,
 )
 urlpatterns = [
     path('', home, name='home'),
     path('list/', student_list, name='student_list'),
+    path('add/', student_add, name='student_add'),
     
-]  
+]   
