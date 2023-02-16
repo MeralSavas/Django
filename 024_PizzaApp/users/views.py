f1bfdfe3b5476cc60ad5d1149120d2771d359df8
MeralSavas <meralsavas@gmail.com>
2023-02-16 19:27:11 +0000

    login auth

--- 024_PizzaApp/users/views.py
+++ 024_PizzaApp/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserForm, LoginForm
+from django.contrib.auth import login
 
 def register(request):
     # form = UserCreationForm()
@@ -21,6 +22,14 @@ def register(request):
 
 def user_login(request):
     form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        
     
     context = {
         "form": form
