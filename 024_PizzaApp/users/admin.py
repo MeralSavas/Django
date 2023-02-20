3e4a463540c4e58eae13aecb2af7d509aa10003a
MeralSavas <meralsavas@gmail.com>
2023-02-20 01:00:01 +0000

    pizzas admin.py

--- 024_PizzaApp/users/admin.py
+++ 024_PizzaApp/users/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Topping, Pizza, Order
 
-
-admin.site.register(Topping)
-admin.site.register(Pizza)
-admin.site.register(Order)
+# Register your models here.
