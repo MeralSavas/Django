7520309cacad47e7989335ffac65e1ef07f81692
MeralSavas <meralsavas@gmail.com>
2023-03-03 00:53:41 +0000

    post serializers

--- Blog_App_Project/blog/serializers.py
+++ Blog_App_Project/blog/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 from .models import Post
 
+import datetime
+
 
 class PostSerializer(serializers.ModelSerializer):
 
+    author = serializers.StringRelatedField()
+    author_id = serializers.IntegerField()
+    image = serializers.StringRelatedField()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = (
+            'id',
+            'image',
+            'title',
+            'content',
+            'is_published',
+            'created_date',
+            'author',
+            'author_id',
+            'slug',
+            
+        )
+
+    def get_created_date(self, obj):
+        return datetime.datetime.strftime(obj.created_date, '%d,%m,%Y')
