268abcaefa54508a2af6e51cbda1fa6cc5676e55
MeralSavas <meralsavas@gmail.com>
2023-03-03 01:10:07 +0000

    comments views

--- Blog_App_Project/blog/views.py
+++ Blog_App_Project/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import PostSerializer
-from .models import Post
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
 
 
 class PostMVS(ModelViewSet):
     queryset = Post.objects.filter(is_published=True)
     serializer_class = PostSerializer
+
+class CommentMVS(ModelViewSet):
+    queryset = Comment.objects.all().select_related('post')
+    serializer_class = CommentSerializer
