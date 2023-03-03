5452399f0c7f72bce2c7b291ef35c5de6c9b1c30
MeralSavas <meralsavas@gmail.com>
2023-03-03 01:15:41 +0000

    views like

--- Blog_App_Project/blog/views.py
+++ Blog_App_Project/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .serializers import PostSerializer, CommentSerializer
-from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer
+from .models import Post, Comment, Like
 
 
 class PostMVS(ModelViewSet):
@@ -11,3 +11,7 @@ class PostMVS(ModelViewSet):
 class CommentMVS(ModelViewSet):
     queryset = Comment.objects.all().select_related('post')
     serializer_class = CommentSerializer
+
+class LikeMVS(ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
