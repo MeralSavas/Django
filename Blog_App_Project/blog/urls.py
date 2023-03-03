7105abb9b1c7095503b6c8185ec215ddf919e36c
MeralSavas <meralsavas@gmail.com>
2023-03-03 01:58:53 +0000

    likes urls.py

--- Blog_App_Project/blog/urls.py
+++ Blog_App_Project/blog/urls.py
@@ -12,6 +12,7 @@ router.register('likes', LikeMVS)
 
 post_base_router = NestedSimpleRouter(router, 'posts', lookup='post')
 post_base_router.register('comments', CommentMVS, basename='comments')
+post_base_router.register('likes', LikeMVS, basename='likes')
 
 urlpatterns = [
     path('', include(router.urls)),
