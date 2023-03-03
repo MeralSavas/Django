2638d4fee25a94a4f697f35964e56bffe55e9577
MeralSavas <meralsavas@gmail.com>
2023-03-03 01:11:20 +0000

    commnets urls.py

--- Blog_App_Project/blog/urls.py
+++ Blog_App_Project/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostMVS
+from .views import PostMVS, CommentMVS
 
 
 router = DefaultRouter()
 
 router.register('posts', PostMVS)
+router.register('comments', CommentMVS)
 
 urlpatterns = [
     path('', include(router.urls)),
