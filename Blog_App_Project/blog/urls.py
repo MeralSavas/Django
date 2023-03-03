2dab241c268b0d947e77e7b6fb1f64548e5bec75
MeralSavas <meralsavas@gmail.com>
2023-03-03 01:16:59 +0000

    like urls.py

--- Blog_App_Project/blog/urls.py
+++ Blog_App_Project/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostMVS, CommentMVS
+from .views import PostMVS, CommentMVS, LikeMVS
 
 
 router = DefaultRouter()
 
 router.register('posts', PostMVS)
 router.register('comments', CommentMVS)
+router.register('likes', LikeMVS)
 
 urlpatterns = [
     path('', include(router.urls)),
