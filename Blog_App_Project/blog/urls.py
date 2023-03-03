d2695767505b5714115c280f138d3ad8ff2ca1c9
MeralSavas <meralsavas@gmail.com>
2023-03-03 01:43:52 +0000

    nested

--- Blog_App_Project/blog/urls.py
+++ Blog_App_Project/blog/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 from .views import PostMVS, CommentMVS, LikeMVS
+from rest_framework_nested.routers import NestedSimpleRouter
 
-
-router = DefaultRouter()
+router = SimpleRouter()
+# router = DefaultRouter()
 
 router.register('posts', PostMVS)
 router.register('comments', CommentMVS)
 router.register('likes', LikeMVS)
 
+post_base_router = NestedSimpleRouter(router, 'posts', lookup='post')
+post_base_router.register('comments', CommentMVS, basename='comments')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(post_base_router.urls)),
 
 ]
