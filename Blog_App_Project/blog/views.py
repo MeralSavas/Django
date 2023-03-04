7ca349c34817cf463f3df65a5a11385145efe90b
MeralSavas <meralsavas@gmail.com>
2023-03-04 02:03:48 +0000

    retrive

--- Blog_App_Project/blog/views.py
+++ Blog_App_Project/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
-from .models import Post, Comment, Like
+from .models import Post, Comment, Like, PostView
 from rest_framework.exceptions import NotFound
 
 from rest_framework.exceptions import ValidationError
@@ -16,6 +16,15 @@ from rest_framework.response import Response
 class PostMVS(ModelViewSet):
     queryset = Post.objects.filter(is_published=True)
     serializer_class = PostSerializer
+
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        postview = PostView.objects.filter(user=request.user, post=instance)
+        if not postview.exists():
+            PostView.objects.create(post=instance, user=request.user)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -82,6 +91,34 @@ class LikeMVS(ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-        
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
 
-        
+        if self.request.user.id == instance.liker.id:
+            if instance.is_liked == True:
+                serializer.validated_data['is_liked'] =False
+            else:
+                serializer.validated_data['is_liked'] = True
+
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)   
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_pk')
+        if post_id == None:
+            return self.queryset
+        else:
+            try:
+                post = Post.objects.get(id=post_id)
+            except Post.DoesNotExist:
+                raise NotFound("A post with this id does not exist")
+        return self.queryset.filter(post = post)    
